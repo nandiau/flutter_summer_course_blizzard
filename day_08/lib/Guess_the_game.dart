542dda2077d3d175a206f73604ee8980f3091090
Blizzard-5 <blizzard-5@Admin-5s-iMac.local>
2023-07-05 13:29:21 +0800

    day -08 game completed

--- day_08/lib/Guess_the_game.dart
+++ day_08/lib/Guess_the_game.dart
@@ -1,21 +1,31 @@
 import 'dart:io';
 import 'dart:math';
-void main(){
+
+void main() {
   final Random random = Random();
-  final int randomNumber = random.nextInt(100) +1;
-  int attempts =0;
-  int guess =0;
+  final int randomNumber = random.nextInt(100) + 1;
+  int attempts = 0;
+  int? guess = 0;
   print('Welcome to Guess the Number!');
   print('I have chosen a number between 1 and 100.');
   print('Can you guess the number?');
-  while  (guess!=randomNumber){
+  while (guess != randomNumber) {
     stdout.write('Enter the guess');
     String? input = stdin.readLineSync();
-    if(input==null){
+    if (input == null) {
       print('Invalid input. Please enter a number.');
-      continue
+      continue;
     }
-
+    guess = int.tryParse(input);
+    if (guess! > randomNumber) {
+      print('Too high');
+    } else if (guess < randomNumber) {
+      print('Too low!');
+    } else {
+      print(
+          'Congratulations!You guessed the correct number in $attempts attempts');
+    }
+    attempts++;
+    print('Thanks for playing!');
   }
-
 }
