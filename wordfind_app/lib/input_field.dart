21491e5e6dcb05d4bfc54cfde4f06783f1f7ae8c
Blizzard-5 <blizzard-5@Admin-5s-iMac.local>
2023-08-04 12:54:41 +0800

    add day_18

--- wordfind_app/lib/input_field.dart
+++ wordfind_app/lib/input_field.dart
@@ -1,8 +1,33 @@
+import 'dart:html';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class InputField extends StatelessWidget {
-  const InputField({super.key});
+class InputField extends StatefulWidget {
+  final void Function(String) onSubmitted;
+  const InputField({super.key, required this.onSubmitted});
+
+  @override
+  State<InputField> createState() => _InputFieldState();
+}
+
+class _InputFieldState extends State<InputField> {
+  late TextEditingController _textEditingController;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _textEditingController = TextEditingController();
+  }
+
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _textEditingController.dispose();
+    super.dispose();
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -10,42 +35,44 @@ class InputField extends StatelessWidget {
       width: 310,
       height: 50,
       child: TextField(
-        onSubmitted: (String.(value) {}),
-      maxLines: 1,
-      style: TextStyle(
-        color: Color(0xFFE86B02),
-        fontSize: 18,
-        fontFamily: 'Nunito',
-        fontWeight: FontWeight.w600,
-      ),
-      decoration: InputDecoration(
-        contentPadding: EdgeInsets.symmetric(horizontal: 25, vertical: 12),
-        filled: true,
-        fillColor: Colors.white,
-        prefixIcon: Icon(
-          Icons.person,
+        onSubmitted: ((String value) {
+          widget.onSubmitted(value);
+          _textEditingController;
+        }),
+        maxLines: 1,
+        style: TextStyle(
           color: Color(0xFFE86B02),
+          fontSize: 18,
+          fontFamily: 'Nunito',
+          fontWeight: FontWeight.w600,
         ),
-        hintText: 'Your Name',
-        helperStyle: TextStyle(
-          color: Color(0xFFE86B02),
-        ),
-        suffixIcon: IconButton(
-          onPressed: () {},
-          icon: Icon(
-            Icons.clear,
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.symmetric(horizontal: 25, vertical: 12),
+          filled: true,
+          fillColor: Colors.white,
+          prefixIcon: Icon(
+            Icons.person,
             color: Color(0xFFE86B02),
           ),
+          hintText: 'Your Name',
+          hintStyle: TextStyle(
+            color: Color(0xFFE86B02),
+          ),
+          suffixIcon: IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.clear,
+              color: Color(0xFFE86B02),
+            ),
+          ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(25),
+            borderSide: BorderSide(
+              width: 0,
+              style: BorderStyle.none,
+            ),
+          ),
         ),
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(25),
-          borderSide: BorderSide(
-            width: 0,
-            style: BorderStyle.none,
-          )
-        )
-
-      ),
       ),
     );
   }
--- wordfind_app/lib/main.dart
+++ wordfind_app/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:wordfind_app/Task_page.dart';
 import 'package:wordfind_app/welcome_page.dart';
+
 
 void main() {
   print('Word Find Game');
@@ -7,7 +9,14 @@ void main() {
   MaterialApp myapp = MaterialApp(
       title: 'Word Find Game',
       theme: ThemeData(fontFamily: 'Ribeye'),
-      home: const WelcomePage());
+      home:WelcomePage(),
+
+
+
+  );
 
   runApp(myapp);
+
+
+
 }
