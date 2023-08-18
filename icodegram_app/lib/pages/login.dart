2db33b898c0a08712f165a1f5f4cc1bda582f3b6
Blizzard-5 <blizzard-5@Admin-5s-iMac.local>
2023-08-18 13:01:37 +0800

    add icodegram_app

--- icodegram_app/lib/pages/login.dart
+++ icodegram_app/lib/pages/login.dart
@@ -1,10 +1,109 @@
 import 'package:flutter/material.dart';
 
-class Login extends StatelessWidget {
-  const Login({super.key});
+import '../component/text_field.dart';
+import '../resources/auth_method.dart';
+import 'home_screen.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  bool _isLoading = false;
+  @override
+  void dispose() {
+    super.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+  }
+
+  void loginUser() async {
+    setState(() {
+      _isLoading = true;
+    });
+    String result = await AuthMethods().loginUser(
+        email: _emailController.text, password: _passwordController.text);
+    if (result == 'success') {
+      setState(() {
+        _isLoading = false;
+      });
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => HomeScreen()));
+
+      print('Logged in');
+    } else {
+      print('Not logged in');
+    }
+    // if{result = 'success';
+    //   print('Logged in');} else {
+    //   print('Not logged in');
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.symmetric(
+            horizontal: 32,
+          ),
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Flexible(
+                flex: 2,
+                child: Container(),
+              ),
+              Text('Icodegram'),
+              SizedBox(
+                height: 64,
+              ),
+              TextFieldInput(
+                  hintText: 'Enter your email',
+                  isPassword: false,
+                  textEditingController: _emailController,
+                  textInputType: TextInputType.emailAddress),
+              SizedBox(
+                height: 24,
+              ),
+              TextFieldInput(
+                  hintText: 'Enter your password',
+                  isPassword: true,
+                  textEditingController: _passwordController,
+                  textInputType: TextInputType.text),
+              SizedBox(
+                height: 64,
+              ),
+              InkWell(
+                  onTap: () {
+                    loginUser();
+                  },
+                  child: _isLoading
+                      ? Center(
+                          child: CircularProgressIndicator(
+                            color: Colors.white,
+                          ),
+                        )
+                      : Center(
+                          child: Text('Нэвтрэх'),
+                        )),
+              SizedBox(
+                height: 12,
+              ),
+              Flexible(
+                flex: 2,
+                child: Container(),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ icodegram_app/lib/resources/auth_method.dart
@@ -0,0 +1,53 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthMethods {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _fireStore = FirebaseFirestore.instance;
+
+  Future<String> loginUser({
+    required String email,
+    required String password,
+  }) async {
+    String result = 'Some error occured';
+    try {
+      if (email.isNotEmpty || password.isNotEmpty) {
+        await _auth.signInWithEmailAndPassword(
+            email: email, password: password);
+        result = 'success';
+      } else {
+        result = 'Please enter all the fields';
+      }
+    } catch (error) {
+      result = error.toString();
+    }
+
+    return result;
+  }
+
+  Future<String> signUpUser({
+    required String email,
+    required String password,
+    required String username,
+  }) async {
+    String result = 'Some error occured';
+    try {
+      if (email.isNotEmpty || password.isNotEmpty || username.isNotEmpty) {
+        UserCredential credential = await _auth.createUserWithEmailAndPassword(
+            email: email, password: password);
+
+        _fireStore.collection('users').doc(credential.user!.uid).set({
+          'username': username,
+          'uid': credential.user!.uid,
+          'email': email,
+          'following': [],
+          'followers': []
+        });
+        result = 'success';
+      }
+    } catch (err) {
+      result = err.toString();
+    }
+    return result;
+  }
+}
